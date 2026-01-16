7e68240e20137b4a84183a064f62f9df41450c7d
MD JAVED ALI <mdjavedali67990@gmail.com>
2026-01-17 02:28:57 +0530

    Added Error Page

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { HomePage } from './pages/HomePage'
 import { CheckoutPage } from './pages/CheckoutPage/CheckoutPage'
 import { Orders } from './pages/Orders'
 import { Tracking } from './pages/Tracking'
+import { NotFoundPage } from './pages/NotFoundPage'
 import './App.css'
 
 function App() {
@@ -13,6 +14,7 @@ function App() {
         <Route path='checkout' element={<CheckoutPage />} />
         <Route path='orders' element={<Orders />} />
         <Route path='tracking' element={<Tracking />} />
+        <Route path='*' element={<NotFoundPage />} />
       </Routes>
     </>
   )
---
+++ src/pages/NotFoundPage.jsx
@@ -0,0 +1,16 @@
+import { Header } from "../components/Header";
+import "./NotFoundPage.css";
+
+export function NotFoundPage() {
+    return (
+        <>
+        <title>404-Error</title>
+
+        <Header />
+
+        <div className="message-body">
+          <div className="message">Page not found</div>
+        </div>
+        </>
+    )
+}
