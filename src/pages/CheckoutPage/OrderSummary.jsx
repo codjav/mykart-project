73c71b84f6bcd8a06700a3657fa5878c78f3fc1d
MD JAVED ALI <mdjavedali67990@gmail.com>
2026-01-20 02:57:03 +0530

    make delete button work in cart

--- src/pages/CheckoutPage/OrderSummary.jsx
+++ src/pages/CheckoutPage/OrderSummary.jsx
@@ -1,4 +1,5 @@
 import dayjs from "dayjs";
+import axios from "axios";
 import { formatMoney } from "../../utils/money";
 import { DeliveryOptions } from "./DeliveryOptions";
 
@@ -12,6 +13,12 @@ export function OrderSummary({ deliveryOptions, cart, loadCart }) {
               return deliveryOption.id == cartItem.deliveryOptionId;
             },
           );
+
+          const deleteCartItem = async () => {
+            await axios.delete(`/api/cart-items/${cartItem.productId}`);
+            await loadCart();
+          };
+
           return (
             <div key={cartItem.productId} className="cart-item-container">
               <div className="delivery-date">
@@ -39,7 +46,8 @@ export function OrderSummary({ deliveryOptions, cart, loadCart }) {
                     <span className="update-quantity-link link-primary">
                       Update
                     </span>
-                    <span className="delete-quantity-link link-primary">
+                    <span className="delete-quantity-link link-primary"
+                    onClick={deleteCartItem}>
                       Delete
                     </span>
                   </div>
